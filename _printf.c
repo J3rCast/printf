2c7a75669bd16f178a09bc4216028151ecf4eedf
J3rCast <jerson.castro.r@gmail.com>
2022-03-24 18:39:48 -0500

    update printf

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 
 	if (!format || !*format)
 		return (-1);
-	if (format[i] == '%')
+	if (format[i] == '%' && format[i + 1] == '\0')
 		return (-1);
 
 	while (format[i] != '\0')

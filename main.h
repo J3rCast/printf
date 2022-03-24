a9db08d61a206974e25c64dc40f0c38970392bd8
J3rCast <jerson.castro.r@gmail.com>
2022-03-24 12:55:04 -0500

    add header file

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/*LIBRARIES*/
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+/*STRUCTURES*/
+/**
+ * struct functions - struct made to obtain the correct
+ * funtion
+ * @var: variable given in _printf function
+ * @f: function to call after comparition
+ */
+typedef struct functions
+{
+	char *var;
+	int (*f)(va_list);
+} ob_fun;
+/*PROTOTYPES*/
+int print_character(char c);
+int _printf(const char *format, ...);
+int print_string(va_list args);
+int print_c(va_list args);
+int print_percent(va_list args);
+int print_decimal(va_list args);
+int print_intB10(va_list args);
+int (*get_function(const char *format))(va_list);
+#endif

26b5c993cc497c01e254cd1f15d3286d76fefb13
J3rCast <jerson.castro.r@gmail.com>
2022-03-24 12:54:37 -0500

    add get functions

---
+++ get_function.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * get_function - this function provides the
+ * correct function for printing
+ * @format: char to compare
+ * Return: correct funtion
+ */
+int (*get_function(const char *format))(va_list args)
+{
+	int i = 0;
+
+	ob_fun obtain[] = {
+		{"c", print_c},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_decimal},
+		{"i", print_intB10},
+		{NULL, NULL}
+	};
+
+	while (obtain[i].var != NULL)
+	{
+		if (*format == *(obtain[i].var))
+			break;
+		i++;
+	}
+	return (obtain[i].f);
+}
